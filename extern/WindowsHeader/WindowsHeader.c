72a6ed9e1dcbf5576b3a40732c500a475e43698a
Antonio Roberto <antonioroberto1407@gmail.com>
2024-09-23 23:15:53 -0400

    impl RegGetValue to WinHeader

--- extern/WindowsHeader/WindowsHeader.c
+++ extern/WindowsHeader/WindowsHeader.c
@@ -13,3 +13,116 @@ bool WinH_CopyFileA(const char *sourceFile, const char *destFile, bool failIfExi
 bool WinH_CopyFileW(const wchar_t *sourceFile, const wchar_t *destFile, bool failIfExists) {
     return CopyFileW(sourceFile, destFile, failIfExists);
 }
+/*
+LSTATUS RegGetValueA(
+ [in]                HKEY    hkey,
+ [in, optional]      LPCSTR  lpSubKey,
+ [in, optional]      LPCSTR  lpValue,
+ [in, optional]      DWORD   dwFlags,
+ [out, optional]     LPDWORD pdwType,
+ [out, optional]     PVOID   pvData,
+ [in, out, optional] LPDWORD pcbData
+);
+*/
+/*
+RegGetValueW(
+    HKEY_CURRENT_USER,
+    L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+    L"AppsUseLightTheme",
+    RRF_RT_REG_DWORD, // expected value type
+    nullptr,
+    buffer.data(),
+    &cbData);
+*/
+
+long WinH_RegGetValueA(int hKey, const char *subKey, const char *value, uint32_t flags, unsigned long *valueType, char *regValue,
+                       unsigned long *regValueSize) {
+    HKEY key;
+
+    switch (hKey) {
+    case WIN_H_HKEY_CLASSES_ROOT:
+        key = HKEY_CLASSES_ROOT;
+        break;
+    case WIN_H_HKEY_CURRENT_USER:
+        key = HKEY_CURRENT_USER;
+        break;
+    case WIN_H_HKEY_LOCAL_MACHINE:
+        key = HKEY_LOCAL_MACHINE;
+        break;
+    case WIN_H_HKEY_USERS:
+        key = HKEY_USERS;
+        break;
+    case WIN_H_HKEY_PERFORMANCE_DATA:
+        key = HKEY_PERFORMANCE_DATA;
+        break;
+    case WIN_H_HKEY_PERFORMANCE_TEXT:
+        key = HKEY_PERFORMANCE_TEXT;
+        break;
+    case WIN_H_HKEY_PERFORMANCE_NLSTEXT:
+        key = HKEY_PERFORMANCE_NLSTEXT;
+        break;
+    case WIN_H_HKEY_CURRENT_CONFIG:
+        key = HKEY_CURRENT_CONFIG;
+        break;
+    case WIN_H_HKEY_DYN_DATA:
+        key = HKEY_DYN_DATA;
+        break;
+    case WIN_H_HKEY_CURRENT_USER_LOCAL_SETTINGS:
+        key = HKEY_CURRENT_USER_LOCAL_SETTINGS;
+        break;
+    default:
+        key = NULL;
+        break;
+    }
+
+    if (key == NULL)
+        return -1;
+
+    return RegGetValueA(key, subKey, value, flags, valueType, regValue, regValueSize);
+}
+
+long WinH_RegGetValueW(int hKey, const wchar_t *subKey, const wchar_t *value, uint32_t flags, unsigned long *valueType, wchar_t *regValue,
+                       unsigned long *regValueSize) {
+    HKEY key;
+
+    switch (hKey) {
+    case WIN_H_HKEY_CLASSES_ROOT:
+        key = HKEY_CLASSES_ROOT;
+        break;
+    case WIN_H_HKEY_CURRENT_USER:
+        key = HKEY_CURRENT_USER;
+        break;
+    case WIN_H_HKEY_LOCAL_MACHINE:
+        key = HKEY_LOCAL_MACHINE;
+        break;
+    case WIN_H_HKEY_USERS:
+        key = HKEY_USERS;
+        break;
+    case WIN_H_HKEY_PERFORMANCE_DATA:
+        key = HKEY_PERFORMANCE_DATA;
+        break;
+    case WIN_H_HKEY_PERFORMANCE_TEXT:
+        key = HKEY_PERFORMANCE_TEXT;
+        break;
+    case WIN_H_HKEY_PERFORMANCE_NLSTEXT:
+        key = HKEY_PERFORMANCE_NLSTEXT;
+        break;
+    case WIN_H_HKEY_CURRENT_CONFIG:
+        key = HKEY_CURRENT_CONFIG;
+        break;
+    case WIN_H_HKEY_DYN_DATA:
+        key = HKEY_DYN_DATA;
+        break;
+    case WIN_H_HKEY_CURRENT_USER_LOCAL_SETTINGS:
+        key = HKEY_CURRENT_USER_LOCAL_SETTINGS;
+        break;
+    default:
+        key = NULL;
+        break;
+    }
+
+    if (key == NULL)
+        return -1;
+
+    return RegGetValueW(key, subKey, value, flags, valueType, regValue, regValueSize);
+}
--- extern/WindowsHeader/WindowsHeader.h
+++ extern/WindowsHeader/WindowsHeader.h
@@ -4,8 +4,37 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef enum {
+    WIN_H_HKEY_CLASSES_ROOT,
+    WIN_H_HKEY_CURRENT_USER,
+    WIN_H_HKEY_LOCAL_MACHINE,
+    WIN_H_HKEY_USERS,
+    WIN_H_HKEY_PERFORMANCE_DATA,
+    WIN_H_HKEY_PERFORMANCE_TEXT,
+    WIN_H_HKEY_PERFORMANCE_NLSTEXT,
+    WIN_H_HKEY_CURRENT_CONFIG,
+    WIN_H_HKEY_DYN_DATA,
+    WIN_H_HKEY_CURRENT_USER_LOCAL_SETTINGS,
+} WIN_H_ENUM_HKEY;
+
+typedef enum {
+    WIN_H_REG_NONE = 0x00000001,
+    WIN_H_REG_SZ = 0x00000002,
+    WIN_H_REG_EXPAND_SZ = 0x00000004,
+    WIN_H_REG_BINARY = 0x00000008,
+    WIN_H_REG_DWORD = 0x00000010,
+    WIN_H_REG_MULTI_SZ = 0x00000020,
+    WIN_H_REG_QWORD = 0x00000040,
+    WIN_H_DWORD = (WIN_H_REG_BINARY | WIN_H_REG_DWORD),
+    WIN_H_QWORD = (WIN_H_REG_BINARY | WIN_H_REG_QWORD),
+    WIN_H_ANY = 0x0000ffff,
+} WIN_H_REG_TYPE;
+
 bool WinH_SetConsoleOutputCP(uint32_t wCodePageID);
 bool WinH_CopyFileA(const char *sourceFile, const char *destFile, bool failIfExists);
 bool WinH_CopyFileW(const wchar_t *sourceFile, const wchar_t *destFile, bool failIfExists);
-
+long WinH_RegGetValueA(int hKey, const char *subKey, const char *value, uint32_t flags, unsigned long *valueType, char *regValue,
+                       unsigned long *regValueSize);
+long WinH_RegGetValueW(int hKey, const wchar_t *subKey, const wchar_t *value, uint32_t flags, unsigned long *valueType, wchar_t *regValue,
+                       unsigned long *regValueSize);
 #endif /* KXD_WIN_H_ISOLATION */
